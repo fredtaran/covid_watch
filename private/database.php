ab9b696b8ff25d01607586c3ef00a83f183e8288
fredtaran <ftaran04@gmail.com>
2023-03-29 23:06:00 +0800

    UI update

---
+++ private/database.php
@@ -0,0 +1,17 @@
+<?php
+
+class Database {
+    protected $connection;
+    
+    function __constructor() {
+        $this->connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+        if($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+        }
+    }
+
+    function getConnection() {
+        return $this->connection;
+    }
+}
